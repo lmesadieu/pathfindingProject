014fd3cb37da84bdec9830874a3eda07dc816048
varangian111 <raamsaw111>
2019-01-16 19:46:42 -0600

    added the readline function to the helper class. It returns an array of strings, but we can modify that if we need it to return something else.

--- src/Top/Helper.java
+++ src/Top/Helper.java
@@ -1,8 +1,40 @@
 package Top;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Helper {
 	public Helper(){
 
 	}
+	public String[] readData(File dataFile){
+		String[] dataArry = new String[lineCounter(dataFile)]; //gets the number of lines in the data file provided and initializes the array with that value
 
+		try {
+			BufferedReader dataReader = new BufferedReader(new FileReader(dataFile));
+			int i= 0;
+			while (dataReader.readLine() != null){
+				dataArry[i] = dataReader.readLine();
+				i++;
+			}
+			return dataArry;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	private int lineCounter(File f){
+		int lines = 0;
+		try (Scanner infile = new Scanner(f)) {
+			while (infile.hasNextLine()){
+				lines++;
+				infile.nextLine();
+			}
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+		return lines;
+	}
 }
